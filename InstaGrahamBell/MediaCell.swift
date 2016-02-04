15856c7eb2d487594483ed7048a063fe2ecf0e63
Lee Edwards <lee.edwards@teespring.com>
2016-02-03 19:49:37 -0800

    Milestone 3

---
+++ InstaGrahamBell/MediaCell.swift
@@ -0,0 +1,25 @@
+//
+//  MediaCell.swift
+//  InstaGrahamBell
+//
+//  Created by Lee Edwards on 2/3/16.
+//  Copyright © 2016 TomLee. All rights reserved.
+//
+
+import UIKit
+
+class MediaCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- InstaGrahamBell/PhotosViewController.swift
+++ InstaGrahamBell/PhotosViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import AFNetworking
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
 
     var photos: [NSDictionary]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.rowHeight = 320
+        
         let clientId = "e05c462ebd86446ea48a5af73769b602"
         let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
         let request = NSURLRequest(URL: url!)
@@ -30,6 +36,7 @@ class PhotosViewController: UIViewController {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
                             self.photos = responseDictionary["data"] as? [NSDictionary]
+                            self.tableView.reloadData()
                     }
                 }
         });
@@ -40,7 +47,19 @@ class PhotosViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.photos?.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MediaCell") as! MediaCell
+        let photo = self.photos![indexPath.row]
+        let imageUrl = photo["images"]!["standard_resolution"]!!["url"] as! String
+        
+        cell.photoView.setImageWithURL(NSURL(string: imageUrl)!)
+        return cell
+    }
 
 }
 
